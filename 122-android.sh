061961dac10e59e48a659fbfdc530ce468c39815
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 14:05:01 -0600

    Create 122-android.sh

---
+++ 122-android.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#########################
+#     Android
+#########################
+
+sudo apt install android-sdk adb fastboot android-sdk-build-tools android-sdk-common android-sdk-platform-tools -yy
+
