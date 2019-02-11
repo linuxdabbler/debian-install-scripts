cba92f0f8fc6c27a9f123de493a49da6368585b8
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 13:09:43 -0600

    Create 116-video-editing-converting-viewing.sh

---
+++ 116-video-editing-converting-viewing.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+##############################################################
+#         Video Editing, Converting, Viewing
+##############################################################
+
+sudo apt install kdenlive -yy
+
+sudo apt install handbrake -yy
+
+sudo apt install vlc -yy
+
+sudo apt install audacity -yy
