f412b50a258ba1e9aca7882c0da3a6373fa3222d
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 13:55:07 -0600

    Create 120-virtualbox.sh

---
+++ 120-virtualbox.sh
@@ -0,0 +1,16 @@
+#!/bin.sh
+
+sudo chown $USER /etc/apt/sources.list
+
+sudo chmod 755 /etc/apt/sources.list
+
+sudo echo "deb http://ftp.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
+
+sudo chown root:root /etc/apt-sources.list
+
+sudo chmod 600 /etc/apt/sources.list
+
+sudo apt update
+
+sudo apt install virtualbox virtualbox-ext-pack
+
