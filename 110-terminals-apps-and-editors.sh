09e18dc8e16653071ca0179f6e13b3edd47527b9
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 12:52:02 -0600

    Create 110-terminals-apps-and-editors.sh
    install terminals, terminal apps, and text editors

---
+++ 110-terminals-apps-and-editors.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# You can comment out any of these with a "#" before "sudo" and it won't install it
+
+#############################################################
+#         Terminals, Terminal Apps, and Text Editors
+#############################################################
+
+sudo apt install rxvt-unicode urxvt-perls -yy
+
+sudo apt install rager -yy
+
+sudo apt install feh -yy
+
+sudo apt install gdebi -yy
+
+sudo apt install neofetch -yy
+
+sudo apt install screenfetch -yy
+
+sudo apt install figlet -yy
+
+sudo apt install lolcat -yy
+
+sudo apt install vim -yy
+
+sudo apt install nano -yy
+
+sudo apt install geany -yy
+
+sudo apt install build-essential -yy
+
+sudo apt install cmake -yy
+
+sudo apt install p7zip p7zip-full unrar-free unzip -yy
+
+sudo apt install lshw -yy
+
+sudo apt install powerline -yy
+
