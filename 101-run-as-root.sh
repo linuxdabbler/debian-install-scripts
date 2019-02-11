4cecd319f98335a42fb551b83e798d4079e569f6
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 13:56:44 -0600

    Update 101-run-as-root.sh

--- 101-run-as-root.sh
+++ 101-run-as-root.sh
@@ -3,7 +3,7 @@
 apt install sudo
 
 sed -e '/Binary/s/^/#/g' -i /etc/apt/sources.list
-sed -i 's/main/main contrib nonfree/gI' /etc/apt/sources.list
+sed -i 's/main/main contrib non-free/gI' /etc/apt/sources.list
 
 apt update && apt upgrade
 
