67d9ea808b9a8cd169ac44268564d535dbc06f98
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-11 13:27:13 -0600

    Create 117-sounds-and-codecs.sh

---
+++ 117-sounds-and-codecs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+###################################
+#         Sounds and Codecs
+###################################
+
+sudo apt install pulseaudio pulseaudio-utils -yy
+
+sudo apt install pavucontrol -yy
+
+sudo apt install pulseaudio-dlna pulseaudio-equalizer gstreamer1.0-pulseaudio -yy
+
+sudo apt install alsa-utils gstreamer1.0-alsa alsamixergui alsaplayer-gtk -yy
+
+sudo apt install alsa-player-daemon alsa-player-common alsa-player-alsa -yy
+
+sudo apt install libao-common libao-dbd libao-dev libao4 -yy
+
+sudo apt install libasound2 libasound-data libasound-dev libasound-doc libasound-plugins -yy
+
+sudo apt install exfat-utils -yy
+
+
+
+
