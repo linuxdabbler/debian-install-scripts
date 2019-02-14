b8c4d762532585889df29cdc77ce6a92cc2a221c
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-14 15:09:05 -0600

    Update 120-virtualbox.sh

--- 120-virtualbox.sh
+++ 120-virtualbox.sh
@@ -6,7 +6,7 @@ sudo chmod 755 /etc/apt/sources.list
 
 sudo echo "deb http://ftp.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
 
-sudo chown root:root /etc/apt-sources.list
+sudo chown root:root /etc/apt/sources.list
 
 sudo chmod 600 /etc/apt/sources.list
 
