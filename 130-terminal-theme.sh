f0720da2f9d07b3f5b2e0fec622158826161129d
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-14 11:52:36 -0600

    Update 130-terminal-theme.sh

--- 130-terminal-theme.sh
+++ 130-terminal-theme.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "[Configuration]
->ColorCursor=#c7c7ccccd1d1
->ColorForeground=#c7c7ccccd1d1
->ColorBackground=#1c1c20202323
->ColorPalette=#1c1c20202323;#c7c7aeae9595;#9595c7c7aeae;#aeaec7c79595;#aeae9595c7c7;#c7c79595aeae;#9595aeaec7c7;#c7c7ccccd1d1;#74747c7c8484;#c7c7aeae9595;#9595c7c7aeae;#aeaec7c79595;#aeae9595c7c7;#c7c79595aeae;#9595aeaec7c7;#f3f3f4f4f5f5" >> ~/.config/xfce4/terminal/terminalrc
+sudo cat xfce4-terminal-theme > ~/.config/xfce4/terminal/terminalrc
