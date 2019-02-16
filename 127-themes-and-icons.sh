575232843034d420cff21010b7f400821e74c6e7
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-15 18:07:18 -0600

    Update 127-themes-and-icons.sh

--- 127-themes-and-icons.sh
+++ 127-themes-and-icons.sh
@@ -2,6 +2,8 @@
 
 sudo apt install numix-gtk-theme blackbird-gtk-theme -yy
 
+sudo apt-get install gtk2-engines-murrine gtk2-engines-pixbuf
+
 # VIVACIOUS ICONS
 
 wget https://launchpad.net/~ravefinity-project/+archive/ubuntu/ppa/+files/vivacious-colors_1.4~xenial~NoobsLab.com_all.deb
@@ -21,3 +23,13 @@ cd papirus-icon-theme/
 cd ..
 
 sudo rm -r papirus*
+
+git clone https://github.com/vinceliuice/vimix-gtk-themes
+
+cd vimix-gtk-themes
+
+./Install
+
+cd ..
+
+sudo rm -r vimix*
