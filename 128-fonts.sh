3e634e0fcd31bfa48633935aa4eaef9ba4ad214e
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-15 19:41:30 -0600

    Update 128-fonts.sh

--- 128-fonts.sh
+++ 128-fonts.sh
@@ -1,5 +1,7 @@
 sudo mkdir /usr/share/fonts/truetype/ubuntu-fonts
 
+sudo mkdir /usr/share/fonts/truetype/yosemite-san-francisco
+
 wget https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip
 
 unzip *.zip
@@ -13,4 +15,15 @@ cd ..
 rm *.zip
 
 rm -r ubuntu-font-family*
+
+git clone https://github.com/supermarin/YosemiteSanFranciscoFont
+
+cd YosemiteSanFranciscoFont/
+
+sudo mv *.ttf /usr/share/fonts/truetype/yosemite-san-francisco
+
+cd ..
+
+sudo rm -r YosemiteSanFranciscoFont/
+
 
