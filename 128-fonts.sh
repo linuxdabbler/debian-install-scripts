5a5310dfcd1537a737ea4cf129e6f28a6448b97e
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-18 21:49:20 -0600

    added powerline-fonts

--- 128-fonts.sh
+++ 128-fonts.sh
@@ -4,6 +4,8 @@ sudo mkdir /usr/share/fonts/truetype/ubuntu-fonts
 
 sudo mkdir /usr/share/fonts/truetype/yosemite-san-francisco
 
+sudo mkdir /usr/share/fonts/powerline
+
 wget https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip
 
 unzip *.zip
@@ -28,4 +30,10 @@ cd ..
 
 sudo rm -r YosemiteSanFranciscoFont/
 
+git clone https://github.com/powerline/fonts
 
+cd fonts 
+
+./install.sh
+
+sudo cp /home/$USER/.local/share/fonts/*Powerline* /usr/share/fonts/powerline
