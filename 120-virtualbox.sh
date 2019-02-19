9e03db36fd3bda96d08486186bcad8b56cfb4cbf
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-19 13:22:15 -0600

    Update 120-virtualbox.sh

--- 120-virtualbox.sh
+++ 120-virtualbox.sh
@@ -12,6 +12,6 @@ sudo chmod 600 /etc/apt/sources.list
 
 sudo apt update
 
-sudo apt install virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-guest-additions-iso virtualbox-guest-dkms -yy
+sudo apt install virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-guest-additions-iso virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 -yy
 
 sudo usermod -aG vboxusers $USER
