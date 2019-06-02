0c7b543246df76a13a3b67a9f6122297b71e2456
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-06-02 04:49:09 -0500

    Update 130-gaming.sh

--- 130-gaming.sh
+++ 130-gaming.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# fix permissions for scripting
+sudo chown $USER /etc/apt/sources.list.d/
+
 # add 32 bit architecture
 sudo dpkg --add-architecture i386
 
@@ -13,14 +16,14 @@ sudo apt install dxvk dxvk-wine32-development dxvk-wine64-development
 sudo apt install steam
 
 # import wine gpg key
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -nc https://dl.winehq.org/wine-builds/winehq.key
 
 # add wine gpg key
 sudo apt-key add winehq.key
 
 # add wine repository
 sudo touch /etc/apt/sources.list.d/wine.list
-sudo cat "deb https://dl.winehq.org/wine-builds/debian buster main" > /etc/apt/sources.list.d/wine.list
+sudo echo "deb https://dl.winehq.org/wine-builds/debian buster main" > /etc/apt/sources.list.d/wine.list
 
 # update
 sudo apt update
@@ -29,14 +32,18 @@ sudo apt update
 sudo apt install --install-recommends winehq-staging
 
 #import lutris repository key
-wget https://download.opensuse.org/repositories/home:/strycore/Debian_9.0/Release.key
+sudo wget https://download.opensuse.org/repositories/home:/strycore/Debian_9.0/Release.key
 
 # add key with apt
 sudo apt-key add Release.key
 
+# return permissions to root
+sudo chown root:root /etc/apt/sources.list.d/
+
+
 # Add Lutris Repository
-sudo touch /etc/apt/sources.list.d/lutris.list
-sudo cat "deb http://download.opensuse.org/repositories/home:/strycore/Debian_9.0/ ./" > /etc/apt/sources.list.d/lutris.list
+touch /etc/apt/sources.list.d/lutris.list
+sudo echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_9.0/ ./" > /etc/apt/sources.list.d/lutris.list
 
 # update
 sudo apt update
