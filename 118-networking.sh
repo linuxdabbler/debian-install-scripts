f82d70cfa665f504e3f98c16191e9cf65350993b
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-06-18 21:33:13 -0500

    backup smb.conf
    backup smb.conf and extract relevant information to simplify the file for configuration

--- 118-networking.sh
+++ 118-networking.sh
@@ -10,13 +10,29 @@ sudo apt install network-manager-openvpn -yy
 
 sudo apt install network-mangager-openvpn-gnome -yy
 
-sudo apt install samba samba-common samba-libs -yy
+sudo apt install samba samba-common samba-libs cifs-utils libcups2 cups smbclient -yy
 
 sudo apt install gvfs-backends -yy
 
 sudo apt install net-tools -yy
 
 sudo apt install ssh -yy
+
+################################################
+#         Backup smb.conf       
+################################################
+
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
+
+##############################################################
+#   Extract relevant information to simplify smb.conf
+##############################################################
+
+sudo chmod 755 /etc/samba/smb.conf.bak
+
+sudo chmod 755 /etc/samba/smb.conf
+
+sudo grep -v -E "^#|^;" /etc/samba/smb.conf.bak | grep . > /etc/samba/smb.conf
 
 ################################################
 #         Enable SSH and SAMBA
@@ -33,3 +49,5 @@ sudo systemctl start smbd
 sudo systemctl enable nmbd
 
 sudo systemctl start nmbd
+
+
