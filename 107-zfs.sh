797e91b26fc57aca2627c07bc6d066685c3a1c70
linuxdabbler <mike868711@gmail.com>
2019-06-28 21:50:37 -0500

    made it better

--- 107-zfs.sh
+++ 107-zfs.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-sudo apt install libuutil1linux libzfs2linux libzpool2linux parted zfs-dkms zfs-zed zfsutils-linux -yy
+
+# Install headers for  your kernel 
+sudo apt install linux-headers-$(uname -r)" linux-image-amd64 -yy
+
+# Symlink /bin/rm to /usr/bin/rm (uncomment this if you are using Debian Stretch)
+#sudo ln -s /bin/rm /usr/bin/rm
+
+# Install ZFS DKMS and ZFS utilities
+sudo apt install zfs-dkms zfsutils-linux -yy
+
+# Add the kernel Modules
+sudo modprobe zfs
+
+# Enable ZFS services
+sudo systemctl enable zfs.target
+sudo systemctl enable zfs-import cache
+sudo systemctl enable zfs-mount
+sudo systemctl enable zfs-import.target
+sudo systemctl enable zfs-import-scan
+sudo systemctl enable zfs-share
+
+# Restart all ZFS services
+
+sudo systemctl restart zfs-import-cache
+sudo systemctl restart zfs-import-scan
+sudo systemctl restart zfs-mount
+sudo systemctl restart zfs-share
