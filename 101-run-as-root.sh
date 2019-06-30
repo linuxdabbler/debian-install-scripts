16e90cdb02b11331d13a1574065cc90313df5461
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-06-30 16:12:44 -0500

    Update 101-run-as-root.sh

--- 101-run-as-root.sh
+++ 101-run-as-root.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-sed -e '/Binary/s/^/#/g' -i /etc/apt/sources.list
-sed -i 's/main/main contrib non-free/gI' /etc/apt/sources.list
-
-apt install sudo git -yy
-
 #apt-transport-https -yy
 
 #sed -i 's/http/https/gI' /etc/apt/sources.list
-
-apt update && apt upgrade
 
 chmod 755 /etc/sudoers
 
@@ -19,6 +12,3 @@ chmod 600 /etc/sudoers
 
 rm sudouser.txt
 
-apt install firmware-linux -yy
-
-apt install llvm clang -yy
