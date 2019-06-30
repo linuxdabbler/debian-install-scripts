e961e76bc5258b1f223645b7634f8cbb2c53d6a1
linuxdabbler <mike868711@gmail.com>
2019-06-30 12:03:22 -0500

    added stretch backports to sources.list

---
+++ 102-stretch-backports.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo echo "#  Stretch Backports Repository" >> /etc/apt/sources.list
+
+sudo echo "deb http://deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
+
