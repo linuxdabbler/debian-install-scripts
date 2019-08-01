a21c4a6de6de21b9e1b51f50582634b4a7b3532a
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-08-01 12:33:13 -0500

    Update and rename 101-testing-repos.sh to repos/testing.sh

--- 101-testing-repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo chown $USER /etc/apt/sources.list
-
-sudo echo "#  Testing Repository" >> /etc/apt/sources.list
-
-sudo echo "deb http://deb.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
-
-sudo chown root:root /etc/apt/sources.list
