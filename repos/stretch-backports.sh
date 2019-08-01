f7c8d106f330ef1782685a50c579f2d55a46c714
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-08-01 14:24:09 -0500

    Update stretch-backports.sh

--- repos/stretch-backports.sh
+++ repos/stretch-backports.sh
@@ -1,11 +1,3 @@
-#!/bin/sh
+#  Stretch Backports Repository
 
-#sudo chown $USER /etc/apt/sources.list
-
-echo "#  Stretch Backports Repository" >> /etc/apt/sources.list
-
-echo "deb http://deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
-
-#sudo chown root:root /etc/apt/sources.list
-
-apt update
+deb http://deb.debian.org/debian stretch-backports main contrib non-free
