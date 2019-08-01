f8facf06acfb249a9fc453676575e1d75cb3466e
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-08-01 14:25:33 -0500

    Update unstable.sh

--- repos/unstable.sh
+++ repos/unstable.sh
@@ -1,11 +1,4 @@
-#!/bin/sh
+# unstable repos
 
-#sudo chown $USER /etc/apt/sources.list
-
-echo "# unstable repos" >> /etc/apt/sources.list
-echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://ftp.us.debian.org/debian unstable main contrib non-free" >> /etc/apt/sources.list
-
-#sudo chown root:root /etc/apt/sources.list
-
-apt update
+deb http://ftp.us.debian.org/debian unstable main contrib non-free
+deb-src http://ftp.us.debian.org/debian unstable main contrib non-free
