722ee1afa5b74069f82a225e2b42e06a927185d2
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-08-21 11:41:07 -0500

    Update 108-ice-ssb-application.sh

--- 108-ice-ssb-application.sh
+++ 108-ice-ssb-application.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_6.0.5_all.deb 
+sudo wget https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_6.0.5_all.deb 
 
 sudo dpkg -i ice*.deb
 
