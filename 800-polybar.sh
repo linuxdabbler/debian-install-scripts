edc23ea96cfc98f0f4d2df3f0c3f782e3082c3d2
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2020-04-14 15:55:24 -0500

    Update 800-polybar.sh

--- 800-polybar.sh
+++ 800-polybar.sh
@@ -2,20 +2,7 @@
 
 
 # Install Dependencies
-
-sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev -yy
-
-
-sudo apt install libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev -yy
-
-
-sudo apt install libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev -yy
-
-
-sudo apt install libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev -yy
-
-
-sudo apt install libpulse-dev ccache libxcb-composite0 libxcb-composite0-dev -yy
+sudo apt install build-essential cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libjsoncpp-dev libxcb-composite0-dev llvm clang ccache libnl-3-dev -yy
 
 # Download from polybar from github
 
@@ -23,6 +10,12 @@ git clone https://github.com/jaagr/polybar.git
 
 # Change directories into polybar
 
-cd polybar
+cd polybar && ./build.sh
 
-./build.sh
+echo "killall -q polybar" > ~/.config/polybar/launch.sh
+echo "polybar example >>/tmp/polybar1.log 2>&1 &" >> ~/.config/polybar/launch.sh
+echo "Bars launched..." >> ~/.config/polybar/launch.sh
+
+chmod +x ~/.config/polybar/launch.sh
+
+
