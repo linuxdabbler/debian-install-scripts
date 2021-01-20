d3430baa6aef2fec5f7c01b6f97550649ec64428
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-01-20 06:46:31 -0600

    Delete 105-tasksel.sh

--- 105-tasksel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo apt install tasksel blends-tasks astro-tasks education-tasks electronics-tasks games-tasks gis-tasks hamradio-tasks junior-tasks med-tasks multimedia-tasks science-tasks debichem-tasks tasksel-data -yy
