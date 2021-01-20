07b56c69ee13949eef3080d40af8f07189ce59cb
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-01-20 06:12:16 -0600

    Create brave-browser.sh

---
+++ brave-browser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install apt-transport-https curl gnupg -yy
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser -yy
