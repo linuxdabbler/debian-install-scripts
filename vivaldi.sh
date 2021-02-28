cd6645447a0a28dd0d0755144bd0cd2ca105489e
Rafael Moyano <rafamva@yahoo.com.ar>
2021-02-27 23:06:38 -0300

    vivaldi script modified to get the latest amd64.deb version.

--- vivaldi.sh
+++ vivaldi.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.1.1337.47-1_amd64.deb
+URL=https://vivaldi.com/download/
+URL2=https://downloads.vivaldi.com/stable/
 
-sudo dpkg -i vivaldi*.deb
+wget $URL
+
+if [ -e index.html ]
+then
+    new_deb_pkg=$(cat index.html| grep "vivaldi-stable" | awk '{print $30}'| cut -c44-81)
+    echo Downloanding amd64.deb package.
+    wget $URL2 -o $new_deb_pkg
+    status=$?
+    
+    if [ $status -eq 8 ]
+    then 
+	echo File downloaded.
+    fi
+fi
+if [ -e $new_deb_pkg ]
+then
+    echo Installing amd64.deb package.
+    sudo dpkg -i $new_deb_pkg
+else
+   echo Error installing deb package.
+fi
