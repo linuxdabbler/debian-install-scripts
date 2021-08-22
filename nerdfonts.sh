c91fd7eaca44dc5fc03c1780a73c31966ff1acae
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-08-22 07:12:42 -0500

    include Ubuntu & Inconsolata Nerdfonts

---
+++ nerdfonts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Inconsolata.zip
+
+unzip Ubuntu.zip
+
+mkdir -p ~/.local/share/fonts/nerdfonts/ubuntu
+
+mv *.ttf ~/.local/share/fonts/nerdfonts/ubuntu
+
+rm Ubuntu.zip
+
+unzip Inconsolata.zip
+
+mkdir -p ~/.local/share/fonts/nerdfonts/inconsolata
+
+mv *.ttf ~/.local/share/fonts/nerdfonts/inconsolata/
+
+rm Inconsolata.zip
+
+fc-cache -f -v
